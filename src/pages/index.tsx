c865223e6166dfb512e2f6719ff9831e751d221b
Yosipmikecolin <colinparrado@gmail.com>
2023-07-21 16:17:30 -0500

    base structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+      <main>
+        <h1>Pagina de inicio</h1>
+      </main>
+    );
+  }
+  
---
+++ src/pages/products.tsx
@@ -0,0 +1,9 @@
+const Products = () => {
+
+    return (
+        <h2>Porductos</h2>
+    )
+}
+
+
+export default Products;
