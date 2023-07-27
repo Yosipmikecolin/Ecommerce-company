6101171765d9b45a8e2880e03b66b9f2eb1f5ac1
Yosipmikecolin <colinparrado@gmail.com>
2023-07-26 19:58:54 -0500

    settings

--- src/components/slider/utils/index.ts
+++ src/components/slider/utils/index.ts
@@ -1,17 +1,17 @@
 export const sliders = [
     {
       id: 1,
-      url: "https://images.unsplash.com/photo-1684847625002-e61a864ce2e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=80",
+      url: "https://images.unsplash.com/photo-1543672039-3cc28caedb76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1746&q=80",
       text: "Eleva tu estilo con la nueva coleción de chaquetas premium Go",
     },
     {
       id: 2,
-      url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
+      url: "https://images.unsplash.com/photo-1590673846749-e2fb8f655df8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
       text: "Descuento de verano del 30% en nuestra zapatillas Nikes",
     },
     {
       id: 3,
-      url: "https://images.unsplash.com/photo-1685945000440-2b7145b84b9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2664&q=80",
+      url: "https://images.unsplash.com/photo-1630750793250-71173d8bfa5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
       text: "Todos nuestros bolsos y carteras en tan solo $200.000 por tiempo limitado",
     },
   ];
